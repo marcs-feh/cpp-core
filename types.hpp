e966bcf583006d32b9f89f59d3c96b67938823f2
marcs-feh <marcosfehlauer@protonmail.com>
2023-04-14 20:36:33 -0300

    init

---
+++ types.hpp
@@ -0,0 +1,26 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdalign.h>
+
+using i8  = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+using u8  = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+
+using f32 = float;
+using f64 = double;
+
+using byte    = uint8_t;
+using rune    = uint32_t;
+using uintptr = uintptr_t;
+using usize   = size_t;
+#ifndef NO_SIGNED_SIZE
+static_assert(sizeof(ptrdiff_t) == sizeof(size_t),
+              "Could not find suitable ssize. Use NO_SIGNED_SIZE to disable it.");
+using ssize   = ptrdiff_t;
+#endif
