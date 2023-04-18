d20f6f5df3bb2d65ddb50e6f0b16a4d4ebfbca09
marcs-feh <marcosfehlauer@protonmail.com>
2023-04-18 17:08:33 -0300

    add forward

--- utils.hpp
+++ utils.hpp
@@ -10,10 +10,18 @@ struct DelReference<T&> { typedef T Type;};
 template<typename T>
 struct DelReference<T&&>{ typedef T Type;};
 
+// Transforms something into an r-value reference.
 template<typename T>
 typename DelReference<T>::Type&& move(T&& x){
 	typedef typename DelReference<T>::Type&& RvalRef;
 	return static_cast<RvalRef>(x);
+}
+
+// Contitionally moves x, if and only if, x is an r-value reference. Requires
+// passing template type explicitly.
+template<typename T>
+T&& forward(DelReference<T>& x){
+	return static_cast<T&&>(x);
 }
 
 template<typename T>
